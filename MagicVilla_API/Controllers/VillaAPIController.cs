2bcf6fc8ec94b13917d8c7f063e02545d3e867fa
mreshboboyev <mreshboboyev@gmail.com>
2023-12-28 22:36:47 +0500

    Using ActionResult in methods

--- MagicVilla_API/Controllers/VillaAPIController.cs
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -11,16 +11,16 @@ namespace MagicVilla_API.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
 
         //[HttpGet("id")] // https://localhost:7009/api/VillaAPI/id?id=1
         [HttpGet("{id:int}")] // https://localhost:7009/api/VillaAPI/1
-        public VillaDTO GetVilla(int id)
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            return Ok(VillaStore.villaList.FirstOrDefault(x => x.Id == id));
         }
     }
 }
