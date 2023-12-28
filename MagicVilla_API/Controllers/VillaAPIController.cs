af13271dbe376104c33d1388d9ef0cd009f7c182
mreshboboyev <mreshboboyev@gmail.com>
2023-12-28 22:21:17 +0500

    GetVillas() method writed: new Villa() { Id = 1, Name = "Pool View"}, new Villa() { Id = 2, Name = "Beach View"} ; ApiRoute attribute added in VillaAPIController.cs

--- MagicVilla_API/Controllers/VillaAPIController.cs
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_API.Controllers
 {
+    [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>()
+            {
+                new Villa() { Id = 1, Name = "Pool View"}, 
+                new Villa() { Id = 2, Name = "Beach View"}
+            };
+        }
     }
 }
