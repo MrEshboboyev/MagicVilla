3ff87ce7d7e244c40cffe845ea1dfd28802a9127
mreshboboyev <mreshboboyev@gmail.com>
2023-12-29 00:54:50 +0500

    More fields added for Villa.cs and VillaDTO.cs

--- MagicVilla_API/Models/Villa.cs
+++ MagicVilla_API/Models/Villa.cs
@@ -1,9 +1,19 @@
-﻿namespace MagicVilla_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_API.Models
 {
     public class Villa
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
         public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
