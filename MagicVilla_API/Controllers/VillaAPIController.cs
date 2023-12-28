655afc3231910ea7e64e8f5d036c267affc3d1f5
mreshboboyev <mreshboboyev@gmail.com>
2023-12-28 22:23:06 +0500

    GetVillas() method verb is [HttpGet]

--- MagicVilla_API/Controllers/VillaAPIController.cs
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -7,6 +7,7 @@ namespace MagicVilla_API.Controllers
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
         public IEnumerable<Villa> GetVillas()
         {
             return new List<Villa>()
