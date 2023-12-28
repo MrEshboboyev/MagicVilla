d78d0c15a17874f1ed638f18a4c07488bb5e2dab
mreshboboyev <mreshboboyev@gmail.com>
2023-12-28 22:43:19 +0500

    [ProducesResponseType] attribute for UnDocumented

--- MagicVilla_API/Controllers/VillaAPIController.cs
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -19,6 +19,12 @@ namespace MagicVilla_API.Controllers
 
         //[HttpGet("id")] // https://localhost:7009/api/VillaAPI/id?id=1
         [HttpGet("{id:int}")] // https://localhost:7009/api/VillaAPI/1
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(200, Type = typeof(VillaDTO))]
+        //[ProducesResponseType(400)]
+        //[ProducesResponseType(404)]
         public ActionResult<VillaDTO> GetVilla(int id)
         {
             if(id == 0)
